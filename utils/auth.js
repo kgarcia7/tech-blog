3bd9a46b5e01e5a9f5d213fd416075fe7e1d2461
kgarcia7 <kgarcia7814@gmail.com>
2024-02-08 00:23:57 -0500

    adding auth middleware

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// Middleware function to check if the user is authenticated
+const withAuth = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
